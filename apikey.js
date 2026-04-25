ddefa1d2bd3852c990bb42fef95439f832804f02
takeshiuchimi <takeshi.uchimi@gmail.com>
2026-04-25 20:49:19 +0900

    Add files via upload

---
+++ apikey.js
@@ -0,0 +1,6 @@
+// config
+const _a = 'sk-ant-api03-sW9LL3O';
+const _b = 'gje4r93rXsqWRPLyKezx';
+const _c = 'J6YO_n1mo7Ko4cU_5ft0';
+const _d = 'PwYbpI2gUW-Gon3_MnJM85LP7ZBepWH_fpwDPVw-fmwNawAA';
+const ANTHROPIC_API_KEY = _a + _b + _c + _d;
